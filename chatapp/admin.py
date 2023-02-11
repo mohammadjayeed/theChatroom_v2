620e044859b3ceb440b236048397f805538d410c
Mohammad Jayeed <thisisjayeed@gmail.com>
2023-02-12 00:14:25 +0600

    routing and consumer done

---
+++ chatapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import ChatRoom
+
+
+# Register your models here.
+@admin.register(ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
